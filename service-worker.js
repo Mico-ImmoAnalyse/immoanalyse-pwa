acb8aa185ed4b5842548e458bcf1186f700f39fa
Mico-ImmoAnalyse <mico8388@hotmail.com>
2026-03-01 08:50:49 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,46 @@
 const CACHE_NAME = "immoanalyse-v1";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.webmanifest",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+const STATIC_ASSETS = [
+  "/",
+  "/index.html",
+  "/offline.html",
+  "/manifest.webmanifest",
+
+  "/engine/engine.js",
+  "/engine/scoring.js",
+  "/engine/ratios.js",
+  "/engine/garanties.js",
+  "/engine/diagnostic.js",
+  "/engine/crossAnalysis.js",
+  "/engine/ticket.js",
+  "/engine/types.js",
+
+  "/icons/icon-192.png",
+  "/icons/icon-512.png",
+  "/icons/maskable-512.png"
 ];
 
-self.addEventListener("install", (event) => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS))
   );
 });
 
-self.addEventListener("activate", (event) => {
+self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys
-          .filter((k) => k !== CACHE_NAME)
-          .map((k) => caches.delete(k))
-      )
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
     )
   );
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then((resp) => {
-      return (
-        resp ||
-        fetch(event.request).catch(() =>
-          caches.match("./index.html")
-        )
-      );
-    })
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then(r => r || caches.match("/offline.html")))
   );
 });
