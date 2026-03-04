78b2c705a307bbbe5bdbf44805151f39136efd6d
Mico-ImmoAnalyse <mico8388@hotmail.com>
2026-03-04 17:39:19 +0100

    Update sw.js
    Added fetch event listener to prioritize network responses.

--- sw.js
+++ sw.js
@@ -1,7 +1,12 @@
-self.addEventListener("install", () => {
+self.addEventListener("install", event => {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", () => {
+self.addEventListener("activate", event => {
   clients.claim();
+});
+
+// Pas de stratégie de cache agressive : toujours le réseau d'abord
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request));
 });
